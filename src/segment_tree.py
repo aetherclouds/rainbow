c2199a02529f297dce5718900f6e90039faaa3bf
Erik <androydgba@gmail.com>
2023-05-27 20:44:49 -0300

    implemented SumSegmentTree

--- src/segment_tree.py
+++ src/segment_tree.py
@@ -1,154 +1,146 @@
-from typing import Callable
-class SegmentTree:
-    def __init__(self, max_size:int, operation:Callable, neutral_value:float):
+"""Code from https://raw.githubusercontent.com/openai/baselines/master/baselines/common/segment_tree.py"""
+
+import operator
+
+class SegmentTree(object):
+    def __init__(self, capacity, operation, neutral_element):
+        """Build a Segment Tree data structure.
+
+        https://en.wikipedia.org/wiki/Segment_tree
+
+        Can be used as regular array, but with two
+        important differences:
+
+            a) setting item's value is slightly slower.
+               It is O(lg capacity) instead of O(1).
+            b) user has access to an efficient ( O(log segment size) )
+               `reduce` operation which reduces `operation` over
+               a contiguous subsequence of items in the array.
+
+        Paramters
+        ---------
+        capacity: int
+            Total size of the array - must be a power of two.
+        operation: lambda obj, obj -> obj
+            and operation for combining elements (eg. sum, max)
+            must form a mathematical group together with the set of
+            possible values for array elements (i.e. be associative)
+        neutral_element: obj
+            neutral element for the operation above. eg. float('-inf')
+            for max and 0 for sum.
         """
-        Implement segment tree.
+        assert capacity > 0 and capacity & (capacity - 1) == 0, "capacity must be positive and a power of 2."
+        self._capacity = capacity
+        self._value = [neutral_element for _ in range(2 * capacity)]
+        self._operation = operation
 
-        # Parameters 
-        max_size: max n. of leaves (or size of array) Must be a power of two.
-        operation: operation that iterates from leaves to root, taking 2 siblings input.
-        neutral_value: a value that's neutral to the operation. eg: sum -> 0, min -> inf
-        """
-        # check if positive and power of two
-        assert (max_size > 0 and max_size & (max_size - 1) == 0)
-        ''' 
-        why does this work?! well see this:
-        >>> as_b = lambda n: "{:08b}".format(n)
-        >>> as_b(35)
-        '00100011'
-        >>> as_b(35-1)
-        '00100010'
-        >>> as_b(35&35-1)
-        '00100010'
-        >>> as_b(35-1)
-        >>> as_b(32) 
-        '00100000'
-        # note how, only for numbers that are powers of two, you
-        # get a subtraction with the following pattern:
-        >>> as_b(32-1)    
-        '00011111'
-        # so we can exploit that to know what numbers are ^2:
-        >>> as_b(32&32-1) 
-        '00000000'
-        '''
-
-        # note that max_size refers to the n. of leaves
-        self.max_size = max_size
-        self.nodes = [neutral_value for _ in range(2 * max_size)]
-        self.operation = operation
-
-
-    def _navigate(
-        self, left, right, node, node_min, node_max,
-    ):
-        """
-        The function that navigates up the tree. You can visualize each step
-        as moving to children nodes (or climbing down the tree when returning).
-        So at every time-step, there is a link with some `node`.
-
-        # Parameters
-        left: left-end to check for (minimum)
-        right: right-end to check for (maximum)
-        node: current node index, initialized at 1 for root node
-        node_min and node_max correspond to the "span" of some parent node's branches.
-        A node always has these 2 values that are to remain UNCHANGED for that tree structure.
-        However, we calculate them on-demand # TODO find right word for this 
-        node_min:  left end span of childrens of a node
-        node_max:  right end span of childrens of a node
-        
-        NOTE: you may choose to index nodes starting at 0 or 1, either is fine. If you chose 0 you could
-        branch from parent X to children Y, Z:
-        Y = X*2+1
-        Z = X*2+2
-        and return the node:
-        return self.nodes[node] # no need to correct
-        """
-        # print('---')
-        # print('start', left)
-        # print('end', right)
-        # print('node', node)
-        # print('node_start', node_min)
-        # print('node_end', node_max)
-
-        # we've reached the "ideal" node; return it
-        if node_min == left and node_max == right:
-            # print('return')
-            return self.nodes[node]
-        
-        # miximum
-        mid = (node_min + node_max) // 2
-
-        if mid>=right:
-            # left branch
-            # print('left branch')
-            return self._navigate(left, right, node*2, node_min, mid)
-        if left>mid:
-            # print('right branch')
-            # right branch
-            return self._navigate(left, right, node*2+1, mid+1, node_max)
-        if left<=mid<right:
-            # split, we'll need to apply operation between the 2 returning nodes
-            # print('split')
-            return self.operation(
-                # order doesn't matter since operation MUST be commutative (aka order property)
-                # left branch
-                self._navigate(left, mid, node*2, node_min, mid),
-                # right branch
-                self._navigate(mid+1, right, node*2+1, mid+1, node_max)
-            )
+    def _reduce_helper(self, start, end, node, node_start, node_end):
+        if start == node_start and end == node_end:
+            return self._value[node]
+        mid = (node_start + node_end) // 2
+        if end <= mid:
+            return self._reduce_helper(start, end, 2 * node, node_start, mid)
+        else:
+            if mid + 1 <= start:
+                return self._reduce_helper(start, end, 2 * node + 1, mid + 1, node_end)
+            else:
+                return self._operation(
+                    self._reduce_helper(start, mid, 2 * node, node_start, mid),
+                    self._reduce_helper(mid + 1, end, 2 * node + 1, mid + 1, node_end)
+                )
 
     def reduce(self, start=0, end=None):
-        """return node or operation for query parameters [start, end]"""
+        """Returns result of applying `self.operation`
+        to a contiguous subsequence of the array.
+
+            self.operation(arr[start], operation(arr[start+1], operation(... arr[end])))
+
+        Parameters
+        ----------
+        start: int
+            beginning of the subsequence
+        end: int
+            end of the subsequences
+
+        Returns
+        -------
+        reduced: obj
+            result of reducing self.operation over the specified range of array elements.
+        """
         if end is None:
-            end = self.max_size
+            end = self._capacity
         if end < 0:
-            end += self.max_size
+            end += self._capacity
+        end -= 1
+        return self._reduce_helper(start, end, 1, 0, self._capacity - 1)
 
-        return self._navigate(left=start, right=end, node=1, node_min=0, node_max=self.max_size-1)
-
-            
     def __setitem__(self, idx, val):
-        """Start from a leaf and recursively update down to the root."""
-        # NOTE: we leave self.nodes[0] to be EMPTY, that is, we start indexing at 1,
-        # so that it's easier to visualize.
-
-        # leaf node
-        node = self.max_size + idx
-        self.nodes[node] = val
-        # nodes: 3, 4 -> 
-        # work down towards the root (from a upside-down perspective)
-        node //= 2
-        while node != 0:
-            # alias
-            parent_node = node
-            sibling_node_left = node*2
-            sibling_node_right = node*2+1
-            self.nodes[parent_node] = self.operation(
-                self.nodes[sibling_node_left],
-                self.nodes[sibling_node_right]
+        # index of the leaf
+        idx += self._capacity
+        self._value[idx] = val
+        idx //= 2
+        while idx >= 1:
+            self._value[idx] = self._operation(
+                self._value[2 * idx],
+                self._value[2 * idx + 1]
             )
-            node //= 2
+            idx //= 2
 
     def __getitem__(self, idx):
-        return self.nodes[self.capacity + idx]
+        assert 0 <= idx < self._capacity
+        return self._value[self._capacity + idx]
+
+
+class SumSegmentTree(SegmentTree):
+    def __init__(self, capacity):
+        super(SumSegmentTree, self).__init__(
+            capacity=capacity,
+            operation=operator.add,
+            neutral_element=0.0
+        )
+
+    def sum(self, start=0, end=None):
+        """Returns arr[start] + ... + arr[end]"""
+        return super(SumSegmentTree, self).reduce(start, end)
+
+    def find_prefixsum_idx(self, prefixsum):
+        """Find the highest index `i` in the array such that
+            sum(arr[0] + arr[1] + ... + arr[i - i]) <= prefixsum
+
+        if array values are probabilities, this function
+        allows to sample indexes according to the discrete
+        probability efficiently.
+
+        Parameters
+        ----------
+        perfixsum: float
+            upperbound on the sum of array prefix
+
+        Returns
+        -------
+        idx: int
+            highest index satisfying the prefixsum constraint
+        """
+        assert 0 <= prefixsum <= self.sum() + 1e-5
+        idx = 1
+        while idx < self._capacity:  # while non-leaf
+            if self._value[2 * idx] > prefixsum:
+                idx = 2 * idx
+            else:
+                prefixsum -= self._value[2 * idx]
+                idx = 2 * idx + 1
+        return idx - self._capacity
+
 
 class MinSegmentTree(SegmentTree):
     def __init__(self, capacity):
-        super().__init__(
-            max_size=capacity,
+        super(MinSegmentTree, self).__init__(
+            capacity=capacity,
             operation=min,
-            neutral_value=float('inf')
-            )
-    
-    def min(self, start=0, end=None):
-        return super().reduce(start, end)
+            neutral_element=float('inf')
+        )
 
-class SumSegmentTree(SegmentTree):
-    def __init(self, capacity):
-        super().__init__(
-            max_size=capacity,
-            operation=operator.add,
-            neutral_value=0.0
-        )    
-        pass
-        # TODO: implement SumSegmentTree
+    def min(self, start=0, end=None):
+        """Returns min(arr[start], ...,  arr[end])"""
+
+        return super(MinSegmentTree, self).reduce(start, end)
